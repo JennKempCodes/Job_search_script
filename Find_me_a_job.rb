ecff6fcba257ab1bd5f15c8b56493fd60886c13d
Jennifer Kemp <jenn.kemp.codes@gmail.com>
2017-09-21 20:27:18 -0400

    Working on by-passing popups

--- Find_me_a_job.rb
+++ Find_me_a_job.rb
@@ -12,9 +12,23 @@ require 'selenium-webdriver'
 @driver = Selenium::WebDriver.for :firefox
 @driver.navigate.to @initial_website_script_url
 
-element = @driver.find_element(:css,'#what')
+element = @driver.find_element(:css, '#what')
 element.click
 element.clear
 # add a clear whenever you sent input so you are sure of the values
 element.send_keys 'QA'
+element.submit
+
+wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+
+#### Check that the table with the given id is displayed
+div = wait.until {
+   element = @driver.find_element(:id, "prime-popover-close-button")
+   element if element.displayed?
+}
+puts "Test Passed: Book Table found" if table
+
+
+# id="prime-popover-close-button"
+
 
